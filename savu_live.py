71ab1c7bd5efc598a1987d17518ff48235174325
Mark Basham <mark.basham@diamond.ac.uk>
2018-01-13 22:02:08 +0000

    Generalised the pipeline for testing

--- savu_live.py
+++ savu_live.py
@@ -14,22 +14,35 @@ def savu_plugin_runner(x, clazz=None):
     plugin._populate_default_parameters()
     return plugin.process_frames([x])
 
+from savu.plugins.filters.denoise_bregman_filter import DenoiseBregmanFilter
+from savu.plugins.filters.image_interpolation import ImageInterpolation
+from savu.plugins.reshape.downsample_filter import DownsampleFilter
+plugins = [DenoiseBregmanFilter, ImageInterpolation, DownsampleFilter]
+
+
 from Queue import Queue
 input_q = Queue()
-remote_q = client.scatter(input_q)
-from savu.plugins.filters.denoise_bregman_filter import DenoiseBregmanFilter
-breg_q = client.map(savu_plugin_runner, remote_q, clazz=DenoiseBregmanFilter)
-from savu.plugins.filters.image_interpolation import ImageInterpolation
-interp_q = client.map(savu_plugin_runner, breg_q, clazz=ImageInterpolation)
-from savu.plugins.reshape.downsample_filter import DownsampleFilter
-down_q = client.map(savu_plugin_runner, interp_q, clazz=DownsampleFilter)
-result_q = client.gather(down_q)
+
+steps = [client.scatter(input_q)]
+
+for plugin in plugins:
+    steps.append(client.map(savu_plugin_runner, steps[-1], clazz=plugin))
+
+result_q = client.gather(steps[-1])
+
+
+
+#remote_q = client.scatter(input_q)
+#breg_q = client.map(savu_plugin_runner, remote_q, clazz=DenoiseBregmanFilter)
+#interp_q = client.map(savu_plugin_runner, breg_q, clazz=ImageInterpolation)
+#down_q = client.map(savu_plugin_runner, interp_q, clazz=DownsampleFilter)
+#result_q = client.gather(down_q)
 
 print(result_q.qsize()) 
 
 def load_data(q):
     i = 0
-    while i < 100:
+    while i < 10:
         print("putting %i" %(i))
         q.put(np.random.rand(1,500,500))
         sleep(random())
@@ -43,7 +56,7 @@ for i in range(100):
     print("Size of queue is %i" % (result_q.qsize()))
     for j in range(result_q.qsize()):
         data = result_q.get()
-        print("getting item %i from queue, (%i,%i) mean value is %f" % (j, data.shape[0], data.shape[1], data.mean()))
+        print("getting item %i from queue, %i(%i,%i) mean value is %f" % (j, len(data.shape), data.shape[0], data.shape[1], data.mean()))
     sleep(1)
 
  
